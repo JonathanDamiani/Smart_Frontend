17b074ec89bcae89980157332e79cef5beafb860
Jonathan Damiani <jdeandamianiapps@gmail.com>
2021-02-04 22:02:19 -0800

    Initial configuration, setting up Apollo and Styled-Components

---
+++ apollo/apollo.js
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import withApollo from "next-with-apollo";
+
+const GRAPHQL_URL = process.env.API_URL || 'http://localhost:4000';
+
+const link = new HttpLink({
+    uri: `${GRAPHQL_URL}/graphql`,
+    fetch
+});
+
+export default withApollo (
+    ({ initialState }) => {
+        return new ApolloClient({
+            link: link,
+            cache:  new InMemoryCache().restore(initialState || {})
+        })
+    }
+);
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { ApolloProvider } from '@apollo/client';
+import { withApollo } from "../apollo";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const App = ({ Component, pageProps, apollo }) => {
+	return ( 
+		<ApolloProvider client={apollo}>
+			<Head>
+				<title>APP</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} /> 
+		</ApolloProvider>
+	)
 }
 
-export default MyApp
+export default withApollo(App);
